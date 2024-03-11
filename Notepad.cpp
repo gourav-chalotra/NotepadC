e7f66f92e118cf1f10452e4641bfa27200216e9c
gouravchalotra4141@gmail.com <gouravchalotra4141@gmail.com>
2024-03-11 23:50:49 +0530

    v01

---
+++ Notepad.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+// File .Edit. View
+void DisplayUI()
+{
+    cout << "File\tEdit\tView" << endl;
+}
+int main()
+{
+    while(true)
+    {
+        DisplayUI();
+        system("clear");
+    }
+    
+    return 0;
+}
