c2511ff671a24b3a6e4f093ae146357fed92d839
gourav-chalotra <chalgourav82@gmail.com>
2024-03-17 02:56:26 +0530

    adding file manu ui

--- Notepad.cpp
+++ Notepad.cpp
@@ -2,7 +2,17 @@
 #include <conio.h>
 using namespace std;
 // File .Edit. View
-vector<string> Menu = {"Save"};
+list<char> head;
+vector<string> Menu = {"Save", "Exit"};
+
+void displayingDataInputOnScreen()
+{
+    cout << endl;
+    for (auto i : head)
+    {
+        cout << i;
+    }
+}
 void DisplayUI()
 {
     for (int i = 0; i < Menu.size(); i++)
@@ -12,10 +22,51 @@ void DisplayUI()
     cout << "\n\n\n"
          << endl;
 }
+// Used to print the desh board of pennal
 void DisplayUI(int indexMenu)
 {
-    
+    for (int i = 0; i < Menu.size(); i++)
+    {
+        if (indexMenu == i)
+            cout << Menu[i] + "*"
+                 << "\t";
+        else
+            cout << Menu[i] << "\t";
+    }
+    cout << "\n\n\n"
+         << endl;
+
+    displayingDataInputOnScreen();
+    char input;
+    input = getch();
+    indexMenu++;
+    cout << endl;
+    system("cls");
+    while (input == 9)
+    {
+        for (int i = 0; i < Menu.size(); i++)
+        {
+            if (indexMenu == i)
+                cout << Menu[i] + "*"
+                     << "\t";
+            else
+                cout << Menu[i] << "\t";
+        }
+        cout << "\n\n\n"
+             << endl;
+        displayingDataInputOnScreen();
+        indexMenu++;
+        indexMenu = indexMenu % Menu.size();
+        input = getch();
+        if (input == 13)
+        {
+            break;
+        }
+        system("cls");
+    }
+    // Index value par ham check kera ga... konsa user na option click kiya hai
 }
+
 void saveFunction()
 {
     list<string> menu;
@@ -23,23 +74,13 @@ void saveFunction()
 }
 int main()
 {
-    list<char> head;
+    // use to store input value in a list of char's
     char inputString;
     bool check = false;
     while (true)
     {
-        if (check)
-        {
-        }
-        else
-        {
-
-            DisplayUI();
-        }
-        for (auto i : head)
-        {
-            cout << i;
-        }
+        DisplayUI();
+        displayingDataInputOnScreen();
         char input = getch();
         if (input == '\b')
         {
@@ -56,6 +97,7 @@ int main()
         else if (input == 9)
         {
             // tab
+            system("cls");
             DisplayUI(0);
         }
         else if (input == 13)
---
+++ test.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+int main()
+{
+    char a;
+    a = getch();
+    cout << int(a) << endl;
+}
